f8884059262af40d0258dca786b2f0f5f48a0c05
AlinurMammadzada5 <alinurmammadzada@gmail.com>
2025-11-18 21:28:29 +0100

    Testing Sorting Methods

---
+++ src/SortingAlgorithms/Main.java
@@ -0,0 +1,11 @@
+package SortingAlgorithms;
+
+public class Main {
+    public static void main(String[] args) {
+int[] arr = {10,9,8,7,6,5,4,3,2,1};
+//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
+       // BubbleSortAlgorithm.bubbleSort(arr);
+//        SelectionSortAlgorithm.selectionSort(arr);
+        InsertionSortAlgorithm.insertionSort(arr);
+    }
+}
